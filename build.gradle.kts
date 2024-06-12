8ed43325fbadc2720dd242bd4b941c32c8dc37ac
tetsuya-azami <zqianjian35@gmail.com>
2024-06-12 09:47:49 +0900

    create project

---
+++ build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    kotlin("jvm") version "1.9.22"
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("com.michael-bull.kotlin-result:kotlin-result:2.0.0")
+    testImplementation("org.jetbrains.kotlin:kotlin-test")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+kotlin {
+    jvmToolchain(17)
+}
