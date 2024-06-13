98b30496a251fbb6628e43a54a2bb8a8b3a879fc
tetsuya-azami <zqianjian35@gmail.com>
2024-06-13 09:55:25 +0900

    tutorial for toResultOr() and orElse()

--- src/test/kotlin/KotlinResultTest.kt
+++ src/test/kotlin/KotlinResultTest.kt
@@ -1,8 +1,7 @@
-import com.github.michaelbull.result.Err
-import com.github.michaelbull.result.Ok
-import com.github.michaelbull.result.andThen
+import com.github.michaelbull.result.*
 import org.junit.jupiter.api.Test
 
+// https://blog.nnn.dev/entry/2023/06/22/110000
 class KotlinResultTest {
     @Test
     fun andThen() {
@@ -24,7 +23,36 @@ class KotlinResultTest {
         println("failure andThen success: $failureAndThenSuccessResult") // Err(Succeeded at first and failure) → 追加のandThenは何も処理されない
         val failureAndThenFailureResult = failureResult.andThen { it -> Err("$it and failure third") }
         println("failure andThen failure: $failureAndThenFailureResult") // Err(Succeeded at first and failure) → 追加のandThenは何も処理されない
+        val orElseResult = failureResult.orElse { Ok("$it orElse") }
+        println("orElse: $orElseResult")
         println("--- 失敗パターン ---")
         // → andThenではエラー情報をすべて集約することは難しそう
+    }
+
+    @Test
+    fun toResultOr() {
+        println()
+        println()
+        println()
+        println("findUser(1): ${findUser(1)}")
+        println("findUser(5): ${findUser(5)}")
+    }
+
+    data class User(val id: Int, val name: String, val status: PaymentStatus) {
+        enum class PaymentStatus {
+            PAID_USER,
+            FREE_USER;
+        }
+    }
+
+    fun findUser(userId: Int): Result<User, String>{
+        val userList = listOf(
+            User(id = 1, name = "Ally", status = User.PaymentStatus.FREE_USER),
+            User(id = 2, name = "Bob", status = User.PaymentStatus.PAID_USER),
+            User(id = 3, name = "Charles", status = User.PaymentStatus.FREE_USER),
+            User(id = 4, name = "Daniel", status = User.PaymentStatus.PAID_USER),
+        )
+        return userList.find { it.id == userId }
+            .toResultOr { "User is not found." }
     }
 }
